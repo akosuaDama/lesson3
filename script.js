4921eaadf0c26a58b5e38fd2741330ed1ee2a14b
akosuaDama <damarisappiahdanso@gmail.com>
2026-03-26 13:22:24 +0000

    adding conditions

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let name = "Damaris";
-console.log(name);
-let age = 15;
-console.log(age);
+// let name = "Damaris";
+// console.log(name);
+// let age = 15;
+// console.log(age);
 let num1 = 10;
 let num2 = 5;
 console.log(num1 + num2);
@@ -16,3 +16,25 @@ let x = 15;
 console.log(typeof x);
 let d = "Damaris";
 console.log(typeof d);
+//prompt a user to enter a number
+//check if the number is even or odd
+//display that the number the user entered is even or odd
+let number = prompt("Enter a number")
+if (number % 2 == 0){
+    alert(`${number} is even`)
+    console.log(`${number} is even`)
+} else{
+    alert(`${number} is odd`)
+    console.log(`${number} is odd`)
+}
+
+
+let name = prompt("Enter your name")
+let age = prompt("Enter your age")
+if (age >= 18){
+    alert(`Hi ${name} you are ${age}years old hence you can sign up for tiktok`)
+    console.log(`Hi ${name} you are ${age}years old hence you can sign up for tiktok`)
+} else{
+    alert(`Hi ${name} , you are ${age}years old hence you can't sign up for tiktok`)
+    console.log(`Hi ${name} , you are ${age}years old hence you can't sign up for tiktok`)
+};
